d7d6e1fff88faa74ac80a7dda2bc79f6e1800ce1
sstone <samuel.stone@hotmail.co.za>
2016-02-01 17:19:40 +0100

    complete elkrem implementation

--- eclair-demo/src/main/scala/fr/acinq/eclair/crypto/Elkrem.scala
+++ eclair-demo/src/main/scala/fr/acinq/eclair/crypto/Elkrem.scala
@@ -7,59 +7,110 @@ import fr.acinq.bitcoin.{Crypto, BinaryData}
   */
 object Elkrem {
 
-  case class Sender(root: BinaryData, curpos: Int)
+  /**
+    * Sender state
+    *
+    * @param root   secret root
+    * @param height tree height (3 means 8 leaves, 15 nodes total)
+    * @param moves  current position i.e. moves down the tree to get the current hash.
+    *               false means left, true means right.
+    */
+  case class Sender(root: BinaryData, height: Int, moves: Vector[Boolean]) {
+    val numberOfHashes = (1 << height) - 1
 
-  object Sender {
-    def init(root: BinaryData) = Sender(root, 0)
+    def hash = Sender.hash(root, moves)
 
-    def getHash(sender: Sender, index: Int): BinaryData = {
-      if (index == 0) sender.root else hash(getHash(sender, parentIndex(index)), isRightChild(index))
-    }
+    def hasNext = !moves.isEmpty
   }
 
-  case class Receiver(nodes: Map[Int, BinaryData], size: Int)
+  object Sender {
+    /**
+      *
+      * @param root   secret hash root
+      * @param height tree height
+      * @return a Sender object that "points" at the first hash in the tree (i.e. the bottom-left leave)
+      */
+    def init(root: BinaryData, height: Int) = Sender(root, height, Vector.fill(height - 1)(false))
+
+    def next(sender: Sender): Sender = sender.copy(moves = nextMoves(sender.moves, sender.height))
+
+    def hash(root: BinaryData, moves: Seq[Boolean]): BinaryData = moves.foldLeft(root)(Elkrem.hash)
+
+    def hash(root: BinaryData, pos: Int): BinaryData = hash(root, moves(pos))
+  }
+
+  /**
+    * Receiver state
+    *
+    * @param height tree height
+    * @param nodes  node map (index -> hash)
+    * @param moves  current position
+    */
+  case class Receiver(height: Int, nodes: Map[Int, BinaryData], moves: Vector[Boolean]) {
+    def getHash(index: Int): Option[BinaryData] = Receiver.getHash(this, index)
+  }
 
   object Receiver {
-    def init(size: Int) = Receiver(Map.empty[Int, BinaryData], size)
+    def init(height: Int) = Receiver(height, Map.empty[Int, BinaryData], Vector.fill(height - 1)(false))
 
-    def addHash(receiver: Receiver, hash: BinaryData, index: Int): Receiver = {
-      val map = (receiver.nodes + (index -> hash))
-      val map1 = map -- allChildren(index, receiver.size)
-      receiver.copy(nodes = map1)
+    /**
+      * add a new hash to the receiver
+      *
+      * @param receiver receiver stats
+      * @param value    hash
+      * @return an updated receiver state
+      */
+    def addHash(receiver: Receiver, value: BinaryData): Receiver = {
+      val pos = index(receiver.moves)
+      val nodes1 = receiver.nodes + (pos -> value) - leftChildIndex(pos) - rightChildIndex(pos)
+      val moves1 = nextMoves(receiver.moves, receiver.height)
+      receiver.copy(nodes = nodes1, moves = moves1)
     }
 
-    def getHash(receiver: Receiver, index: Int): Option[BinaryData] = {
-      if (index == 0)
-        None
+    /**
+      *
+      * @param receiver receiver state
+      * @param pos      position of the hash in the tree
+      * @return Some(hash) if the hash at this position can be computed, None otherwise
+      */
+    def getHash(receiver: Receiver, pos: Int): Option[BinaryData] = {
+      if (pos == 0) None
       else {
-        receiver.nodes.get(index) match {
+        receiver.nodes.get(pos) match {
           case Some(value) => Some(value)
-          case None => getHash(receiver, parentIndex(index)).map(h => hash(h, isRightChild(index)))
+          case None => getHash(receiver, parentIndex(pos)).map(h => hash(h, isRightChild(pos)))
         }
       }
     }
   }
 
+  def nextMoves(moves: Vector[Boolean], height: Int): Vector[Boolean] = {
+    if (moves.isEmpty) moves
+    else moves.last match {
+      case true => moves.take(moves.size - 1)
+      case false =>
+        val moves1 = moves.updated(moves.size - 1, true)
+        moves1 ++ Vector.fill(height - 1 - moves1.size)(false)
+    }
+  }
+
+  def index(moves: Seq[Boolean]): Int = moves.foldLeft(0)(childIndex)
+
+  def moves(pos: Int): Seq[Boolean] = if (pos == 0) Seq.empty[Boolean] else moves(parentIndex(pos)) :+ isRightChild(pos)
+
   def hashLeft(value: BinaryData): BinaryData = Crypto.hash256(value)
 
   def hashRight(value: BinaryData): BinaryData = Crypto.hash256(value :+ 1.toByte)
 
-  def hash(value: BinaryData, isRightChild: Boolean): BinaryData = {
-    if (isRightChild) hashRight(value) else hashLeft(value)
-  }
+  def hash(value: BinaryData, isRightChild: Boolean): BinaryData = if (isRightChild) hashRight(value) else hashLeft(value)
 
   def parentIndex(pos: Int) = (pos - 1) / 2
 
-  def childrenIndex(pos: Int) = (2 * pos + 1, 2 * pos + 2)
+  def childIndex(pos: Int, isRightChild: Boolean): Int = if (isRightChild) rightChildIndex(pos) else leftChildIndex(pos)
+
+  def leftChildIndex(pos: Int) = 2 * pos + 1
+
+  def rightChildIndex(pos: Int) = 2 * pos + 2
 
   def isRightChild(pos: Int) = (pos % 2) == 0
-
-  def allChildren(pos: Int, size: Int, acc: Seq[Int] = Seq.empty[Int]): Seq[Int] = {
-    if (pos >= size / 2)
-      acc
-    else {
-      val (l, r) = childrenIndex(pos)
-      l +: (r +: (acc ++ allChildren(l, size) ++ allChildren(r, size)))
-    }
-  }
 }
--- eclair-demo/src/test/scala/fr/acinq/eclair/crypto/ElkremSpec.scala
+++ eclair-demo/src/test/scala/fr/acinq/eclair/crypto/ElkremSpec.scala
@@ -13,36 +13,56 @@ class ElkremSpec extends FunSuite {
   import Elkrem._
 
   test("sender can compute hashes") {
-    val root: BinaryData = "this is the root".getBytes("UTF-8")
-    val sender = Sender.init(root)
-    assert(Sender.getHash(sender, 0) == root)
-    assert(Sender.getHash(sender, 5) == hashLeft(hashRight(root)))
-    assert(Sender.getHash(sender, 10) == hashRight(hashRight(hashLeft(root))))
+    val root = BinaryData("5a5f20f2b8501b9bc922e0763968fca05f276c298c101eaa7cb6c88ba02e0b3b").reverse
+    val sender = Sender.init(root, 4)
+
+    def hashes(sender: Sender, max: Int, acc: Seq[BinaryData] = Seq.empty[BinaryData]) : Seq[BinaryData] = {
+      if (acc.size == max) acc else hashes(Sender.next(sender), max, acc :+ sender.hash)
+    }
+
+    val results = hashes(sender,15)
+    val expected: Seq[BinaryData] = Seq(
+      "337d823876c6db822bbe63492aef42f8aeafd963ebd8b2e4dd26f787ad70fb66",
+      "1a4ea5614e7b59f5c9781f99e7654d89fefd5aac3c10f76405252cb911ca1bec",
+      "403f1a1f328f6935bc2f5663b92582f48dd3a16e0ebe315a7de1c4c368620f6d",
+      "44228228872eb996e40b37395b1b1cd4845f7c31263958f7f10ebf46f0114568",
+      "9dba0c7e76e7c5c6b4680f4163429706f69b0e490f9f969b3cd53b736aaddb37",
+      "b7459906d6ffaa584bb517455837fd0d8c0a30dba8ba9c424e3142376f96ce93",
+      "d32d85d39660870e7e3911ac1dbb82295d66141b782d1d156ea8002ea800ac6c",
+      "8e113c41eb6f12f705bdfb6b0e1e7a957873f318415fc434b9dcac90967bd067",
+      "b87caafd83f0e5a8568de6535b9d11030218faf92eb1e13a92314ac042094e68",
+      "4a0c0f7af16ac95eef46a3a76e62a34de1ba8576e38a3d7ecda4b296f13103cb",
+      "dc2754c86920f8a79796593622f88eb0783a4660e5a8e557fb1c3fa868445dd1",
+      "2119e994c7389617a0f3833e153e4dc26c19a317182614dc4d5ec4d8ee06e525",
+      "ba76d25c8239f49be12f8cffca9d17403c8e6eaee5ef6d59b85870decdad2915",
+      "24a08d0e279f89032ccbd799eb6c3b9f9d7387178ea2d5edbf3b74b608facf84",
+      "5a5f20f2b8501b9bc922e0763968fca05f276c298c101eaa7cb6c88ba02e0b3b"
+    )
+    val expected1 = expected.map(_.reverse).map(BinaryData(_))
+    assert(results === expected1)
   }
   test("receiver can compute hashes") {
-    val root: BinaryData = "this is the root".getBytes("UTF-8")
-    val sender = Sender.init(root)
-    val receiver = Receiver.init(15)
+    val root = BinaryData("5a5f20f2b8501b9bc922e0763968fca05f276c298c101eaa7cb6c88ba02e0b3b").reverse
+    val sender = Sender.init(root, 4)
+    val receiver = Receiver.init(4)
 
-    val receiver1 = Receiver.addHash(receiver, Sender.getHash(sender, 14), 14)
-    assert(receiver1.nodes(14) == Sender.getHash(sender, 14))
-    val receiver2 = Receiver.addHash(receiver1, Sender.getHash(sender, 13), 13)
-    assert(receiver2.nodes(13) == Sender.getHash(sender, 13))
+    val receiver1 = Receiver.addHash(receiver, sender.hash)
+    val sender1 = Sender.next(sender)
+    assert(receiver1.nodes(7) === Sender.hash(sender1.root, 7))
+    assert(receiver1.getHash(7) === Some(Sender.hash(sender1.root, 7)))
 
-    val receiver3 = Receiver.addHash(receiver2, Sender.getHash(sender, 6), 6)
-    assert(receiver3.nodes(6) == Sender.getHash(sender, 6))
-    assert(receiver3.nodes.get(13).isEmpty)
-    assert(receiver3.nodes.get(14).isEmpty)
-    assert(Receiver.getHash(receiver3, 13) == Some(Sender.getHash(sender, 13)))
-    assert(Receiver.getHash(receiver3, 14) == Some(Sender.getHash(sender, 14)))
+    val receiver2 = Receiver.addHash(receiver1, sender1.hash)
+    val sender2 = Sender.next(sender1)
+    assert(receiver2.nodes(8) === Sender.hash(sender2.root, 8))
+    assert(receiver2.getHash(7) === Some(Sender.hash(sender1.root, 7)))
 
-    val receiver4 = Receiver.addHash(receiver3, Sender.getHash(sender, 10), 10)
-    assert(Receiver.getHash(receiver4, 10) == Some(Sender.getHash(sender, 10)))
-    val receiver5 = Receiver.addHash(receiver4, Sender.getHash(sender, 1), 1)
-    assert(Receiver.getHash(receiver5, 1) == Some(Sender.getHash(sender, 1)))
-    assert(receiver5.nodes.get(10).isEmpty)
-    assert(Receiver.getHash(receiver5, 4) == Some(Sender.getHash(sender, 4)))
-    assert(Receiver.getHash(receiver5, 9) == Some(Sender.getHash(sender, 9)))
-    assert(Receiver.getHash(receiver5, 10) == Some(Sender.getHash(sender, 10)))
+    val receiver3 = Receiver.addHash(receiver2, sender2.hash)
+    val sender3 = Sender.next(sender2)
+    assert(receiver3.nodes(3) === Sender.hash(sender3.root, 3))
+    assert(receiver3.nodes.get(7).isEmpty)
+    assert(receiver3.nodes.get(8).isEmpty)
+    assert(receiver3.getHash(3) === Some(Sender.hash(sender3.root, 3)))
+    assert(receiver3.getHash(7) === Some(Sender.hash(sender3.root, 7)))
+    assert(receiver3.getHash(8) === Some(Sender.hash(sender3.root, 8)))
   }
 }
